3b3493087de8ee590c3140cd1471b008301194d3
aciko11 <the.aciko@gmail.com>
2019-08-27 16:57:09 +0200

    start of the last edit

--- Scripts/alterTables.php
+++ Scripts/alterTables.php
@@ -6,7 +6,7 @@
     global $connect;
 
     $table = "tac";
-    $columns = array("aorta_Diam", "iliacaComSx_Stenosi", "iliacaComDx_Stenosi");
+    //$columns = array("aorta_Diam", "iliacaComSx_Stenosi", "iliacaComDx_Stenosi");
     $columns = array();
 
     $col = new Column;
@@ -55,7 +55,45 @@
         if(mysqli_error($connect) != NULL){
             echo(mysqli_error($connect));
         }
-    } 
+    }
+    echo("<BR>");
     
+    $table = "intervento";
+    $columns = array();
+
+    $col = new Column;
+    $col->colName = "accessoChirurgico";
+    $col->colValue = "varchar(2)";
+    $columns[] = $col;
+
+    $col = new Column;
+    $col->colName = "accessoPercutaneo";
+    $col->colValue = "varchar(2)";
+    $columns[] = $col;
+
+    $col = new Column;
+    $col->colName = "accessoBrachiale";
+    $col->colValue = "varchar(2)";
+    $columns[] = $col;
+
+    $query = array();
+    foreach($columns as $col){
+        $query = "ALTER TABLE ".$table." ADD ".$col->colName." ".$col->colValue;
+        echo($query."<BR><BR>");
+        mysqli_query($connect, $query); //or die(mysqli_error($connect));
+        if(mysqli_error($connect) != NULL){
+            echo(mysqli_error($connect));
+        }
+    }
+
+    $table = "tipo_complicanza";
+    $desc = "Endoleak di tipo I-migrato-ND";
+
+    $query = "INSERT INTO ".$table." (descrizione) VALUES ('".$desc."')";
+    echo($query."<BR><BR>");
+    mysqli_query($connect, $query);
+    if(mysqli_error($connect) != NULL){
+        echo(mysqli_error($connect));
+    }
 
 ?>
